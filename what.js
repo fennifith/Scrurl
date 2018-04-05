b566ad34aa2524a77e5de885cf8d4edf324704b5
James Fenn <18jafenn90@gmail.com>
2018-04-04 21:21:56 -0400

    fix #1

--- what.js
+++ what.js
@@ -1,19 +1,22 @@
 var What = {};
-What.display = function(text, speed, offset) {
-	if (text != null && speed == 0 && offset >= 0) {
+What.display = function(text, options, offset) {
+	if (text != null && offset != null && offset >= 0) {
+		offset *= options.scrollamount != null ? Math.abs(options.scrollamount) : 1;
 		offset %= text.length;
+		if (options.direction == null || options.direction == "right")
+			offset = text.length - offset;
+		
 		var start = text.substring(0, offset).split(" ").join("_");
 		var end = text.substring(offset, text.length).split(" ").join("_");
-		//console.log(start + "___" + end);
 		window.history.replaceState({}, "", "?" + end + "_" + start);
-	} else if (text != null && speed > 0) {
+	} else if (text != null) {
 		What.interval = setInterval(function() {
 			if (What.offset != null)
 				What.offset++;
 			else What.offset = 0;
 
-			What.display(text, 0, What.offset);
-		}, speed);
+			What.display(text, options, What.offset);
+		}, options.scrolldelay != null && options.scrolldelay > 0 ? options.scrolldelay : 200);
 	} else {
 		clearInterval(What.interval);
 	}
